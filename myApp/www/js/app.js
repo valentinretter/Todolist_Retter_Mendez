4d95fda5b1ddb080cf5d6528fb577ab5a3dc3aec
Valentin <valentin.retter9@etu.univ-lorraine.fr>
2020-03-30 12:39:58 +0200

    Ajout du projet

---
+++ myApp/www/js/app.js
@@ -0,0 +1,25 @@
+// App logic.
+window.myApp = {};
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  // Initialsiation
+  if (myApp.controllers.hasOwnProperty(page.id)) {
+    myApp.controllers[page.id](page);
+  }
+
+  // Fill the lists with initial data when the pages we need are ready.
+  // This only happens once at the beginning of the app.
+  if (page.id === 'menuPage' || page.id === 'pendingTasksPage') {
+    if (document.querySelector('#menuPage')
+      && document.querySelector('#pendingTasksPage')
+      && !document.querySelector('#pendingTasksPage ons-list-item')
+    ) {
+      //storage.setItem("data",JSON.stringify(myApp.services.fixtures)); //POUR RE-REMPLIR LE TABLEAU
+      JSON.parse(storage.getItem("data")).forEach(function (data) {
+        myApp.services.tasks.create(data);
+      });
+    }
+  }
+});
---
+++ myApp/www/js/controllers.js
@@ -0,0 +1,61 @@
+/***********************************************************************
+ * App Controllers. These controllers will be called on page initialization. *
+ ***********************************************************************/
+
+myApp.controllers = {
+
+  //////////////////////////
+  // Tabbar Page Controller //
+  //////////////////////////
+  tabbarPage: function(page) {
+    // Set button functionality to open/close the menu.
+    page.querySelector('[component="button/menu"]').onclick = function() {
+      document.querySelector('#mySplitter').left.toggle();
+    };
+
+    // Set button functionality to push 'new_task.html' page.
+    Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-task"]'), function(element) {
+      element.onclick = function() {
+        document.querySelector('#myNavigator').pushPage('html/new_task.html');
+      };
+
+      element.show && element.show(); // Fix ons-fab in Safari.
+    });
+  },
+
+  ////////////////////////////
+  // New Task Page Controller //
+  ////////////////////////////
+  newTaskPage: function(page) {
+    // Set button functionality to save a new task.
+    Array.prototype.forEach.call(page.querySelectorAll('[component="button/save-task"]'), function(element) {
+      element.onclick = function() {
+        var newTitle = page.querySelector('#title-input').value;
+
+        if (newTitle) {
+          // If input title is not empty, create a new task.
+          var tab = {
+            title: newTitle,
+            category: page.querySelector('#category-input').value,
+            description: page.querySelector('#description-input').value,
+            checked: false,
+            state : "pending"
+          };
+          var tabData = JSON.parse(storage.getItem("data"));
+          tabData.push(tab);
+          storage.setItem("data",JSON.stringify(tabData));
+          myApp.services.tasks.create(tab);
+
+          // Set selected category to 'All', refresh and pop page.
+          document.querySelector('#default-category-list ons-list-item ons-radio').checked = true;
+          document.querySelector('#myNavigator').popPage();
+
+        } else {
+          // Show alert if the input title is empty.
+          ons.notification.alert('You must provide a task title.');
+        }
+      };
+    });
+  }
+
+};
